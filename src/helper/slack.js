661156a81d25bfb41f0e2e47e531082afe013b1d
Christian Korndoerfer <christian@perspective.co>
2022-09-15 17:28:50 +0200

    upadte slack webhook package

--- src/helper/slack.js
+++ src/helper/slack.js
@@ -1,71 +1,70 @@
-const Slack = require('slack-node');
+const { IncomingWebhook } = require('@slack/webhook');
 const core = require('@actions/core');
 
-const informSlack = (release) => {
+const informSlack = async (release) => {
     console.log('Informing Slack...');
     const slack = new Slack();
 
-    slack.setWebhook(core.getInput('slack-webhook-url'));
+    const webhook = new IncomingWebhook(core.getInput('slack-webhook-url'));
 
-    slack.webhook(
-        {
-            text: `Version ${release.name} was released :rocket:`,
-            blocks: [
-                {
-                    type: 'header',
-                    text: {
-                        type: 'plain_text',
-                        text: `Version ${release.name} was released :rocket:`,
-                    },
-                },
-                {
-                    type: 'divider',
-                },
-                {
-                    type: 'section',
-                    text: {
-                        type: 'mrkdwn',
-                        text: '*Changelog* :notebook_with_decorative_cover:',
-                    },
-                },
-                {
-                    type: 'section',
-                    text: {
-                        type: 'mrkdwn',
-                        text: release.body,
-                    },
-                },
-                {
-                    type: 'divider',
-                },
-                {
-                    type: 'section',
-                    text: {
-                        type: 'mrkdwn',
-                        text: 'Check it out!',
-                    },
-                    accessory: {
-                        type: 'button',
-                        text: {
-                            type: 'plain_text',
-                            text: 'Visit',
-                            emoji: true,
-                        },
-                        value: 'visit',
-                        url: 'https://next.perspective.com',
-                        action_id: 'button-action',
-                    },
-                },
-            ],
-        },
-        (err, response) => {
-            if (err) {
-                console.warn(err);
-            } else {
-                console.log('Slack response:', response);
-            }
-        },
-    );
+    IncomingWebhook(core.getInput('slack-webhook-url'));
+
+    try {
+        await webhook.send(
+          {
+              text: `Version ${release.name} was released :rocket:`,
+              blocks: [
+                  {
+                      type: 'header',
+                      text: {
+                          type: 'plain_text',
+                          text: `Version ${release.name} was released :rocket:`,
+                      },
+                  }
+                  //     {
+                  //         type: 'divider',
+                  //     },
+                  //     {
+                  //         type: 'section',
+                  //         text: {
+                  //             type: 'mrkdwn',
+                  //             text: '*Changelog* :notebook_with_decorative_cover:',
+                  //         },
+                  //     },
+                  //     {
+                  //         type: 'section',
+                  //         text: {
+                  //             type: 'mrkdwn',
+                  //             text: release.body,
+                  //         },
+                  //     },
+                  //     {
+                  //         type: 'divider',
+                  //     },
+                  //     {
+                  //         type: 'section',
+                  //         text: {
+                  //             type: 'mrkdwn',
+                  //             text: 'Check it out!',
+                  //         },
+                  //         accessory: {
+                  //             type: 'button',
+                  //             text: {
+                  //                 type: 'plain_text',
+                  //                 text: 'Visit',
+                  //                 emoji: true,
+                  //             },
+                  //             value: 'visit',
+                  //             url: 'https://next.perspective.com',
+                  //             action_id: 'button-action',
+                  //         },
+                  //     },
+              ],
+          }
+        );
+    } catch(err) {
+        throw err
+    }
 };
 
 module.exports = informSlack;
