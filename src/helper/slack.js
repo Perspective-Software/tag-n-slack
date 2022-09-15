604b2bb8d4c7632b757624fd9af9261a181e04e2
Christian Korndoerfer <christian@perspective.co>
2022-09-15 17:37:12 +0200

    formatting

--- src/helper/slack.js
+++ src/helper/slack.js
@@ -4,63 +4,63 @@ const core = require('@actions/core');
 const informSlack = async (release) => {
     console.log('Informing Slack...');
 
-    const webhook = new IncomingWebhook(core.getInput('slack-webhook-url'));
+    const webhook = new IncomingWebhook(core.getInput('slack-webhook-url'), {
+        icon_emoji: ':perspective-power2:',
+    });
 
     try {
-        await webhook.send(
-          {
-              text: `Version ${release.name} was released :rocket:`,
-              blocks: [
-                  {
-                      type: 'header',
-                      text: {
-                          type: 'plain_text',
-                          text: `Version ${release.name} was released :rocket:`,
-                      },
-                  }
-                  //     {
-                  //         type: 'divider',
-                  //     },
-                  //     {
-                  //         type: 'section',
-                  //         text: {
-                  //             type: 'mrkdwn',
-                  //             text: '*Changelog* :notebook_with_decorative_cover:',
-                  //         },
-                  //     },
-                  //     {
-                  //         type: 'section',
-                  //         text: {
-                  //             type: 'mrkdwn',
-                  //             text: release.body,
-                  //         },
-                  //     },
-                  //     {
-                  //         type: 'divider',
-                  //     },
-                  //     {
-                  //         type: 'section',
-                  //         text: {
-                  //             type: 'mrkdwn',
-                  //             text: 'Check it out!',
-                  //         },
-                  //         accessory: {
-                  //             type: 'button',
-                  //             text: {
-                  //                 type: 'plain_text',
-                  //                 text: 'Visit',
-                  //                 emoji: true,
-                  //             },
-                  //             value: 'visit',
-                  //             url: 'https://next.perspective.com',
-                  //             action_id: 'button-action',
-                  //         },
-                  //     },
-              ],
-          }
-        );
-    } catch(err) {
-        throw err
+        await webhook.send({
+            text: `Version ${release.name} was released :rocket:`,
+            blocks: [
+                {
+                    type: 'header',
+                    text: {
+                        type: 'plain_text',
+                        text: `Version ${release.name} was released :rocket:`,
+                    },
+                },
+                {
+                    type: 'divider',
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: '*Changelog* :notebook_with_decorative_cover:',
+                    },
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: release.body,
+                    },
+                },
+                {
+                    type: 'divider',
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: 'Check it out!',
+                    },
+                    accessory: {
+                        type: 'button',
+                        text: {
+                            type: 'plain_text',
+                            text: 'Visit',
+                            emoji: true,
+                        },
+                        value: 'visit',
+                        url: 'https://next.perspective.com',
+                        action_id: 'button-action',
+                    },
+                },
+            ],
+        });
+    } catch (err) {
+        throw err;
     }
 };
 
