b7d2b76fefd14e1a70d565a246a4389cbdc2f994
Christian Korndoerfer <christian@perspective.co>
2022-09-15 17:10:07 +0200

    build

--- action/index.js
+++ action/index.js
@@ -88720,16 +88720,15 @@ const informSlack = (release) => {
 
     slack.setWebhook(core.getInput('slack-webhook-url'));
 
-    console.log('release', release);
-
     slack.webhook(
         {
+            fallback: `Version ${release.name} was released :rocket:`,
             blocks: [
                 {
                     type: 'header',
                     text: {
                         type: 'plain_text',
-                        text: `Version ${release.name} was released.`,
+                        text: `Version ${release.name} was released :rocket:`,
                     },
                 },
                 {
@@ -88739,7 +88738,7 @@ const informSlack = (release) => {
                     type: 'section',
                     text: {
                         type: 'mrkdwn',
-                        text: '*Changelog*',
+                        text: '*Changelog* :notebook_with_decorative_cover:',
                     },
                 },
                 {
@@ -88747,6 +88746,27 @@ const informSlack = (release) => {
                     text: {
                         type: 'mrkdwn',
                         text: release.body,
+                    },
+                },
+                {
+                    type: 'divider',
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: 'Check it out!',
+                    },
+                    accessory: {
+                        type: 'button',
+                        text: {
+                            type: 'plain_text',
+                            text: 'Visit',
+                            emoji: true,
+                        },
+                        value: 'visit',
+                        url: 'https://next.perspective.com',
+                        action_id: 'button-action',
                     },
                 },
             ],
