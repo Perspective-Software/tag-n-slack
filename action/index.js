f0bf8a3d3d743ddf62aaf113ed027b5a061a9f5b
Christian Korndoerfer <christian@perspective.co>
2022-09-16 12:19:16 +0200

    refactor

--- action/index.js
+++ action/index.js
@@ -39682,28 +39682,26 @@ const run = async () => {
     let releases = [];
     let release = {};
 
-    // fetch existing releases
     try {
         // Get version from package.json
         const version = packageUtils.getPackageVersion(core.getInput('package-json-path'));
 
+        // fetch existing releases
         releases = await releaseUtils.fetchGithubReleases();
 
         // Don't create release if it already exists
         if (releases.indexOf(`v${version}`) > -1) {
-            console.log('Skipping: Release already exists');
-
-            throw 'Release already exists';
+            throw 'Skipping: Release already exists';
         }
 
         release = await releaseUtils.createGithubRelease();
 
         // Notify Slack about the release
-        informSlack(release);
+        await informSlack(release);
 
         return release;
     } catch (err) {
-        console.warn(err)
+        console.warn(err);
     }
 };
 
